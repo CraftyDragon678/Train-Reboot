90050dccebe4fcd63446a444bf3ed5409623d24a
CraftyDragon678 <craftydragon678@gmail.com>
2020-02-02 13:26:51 +0900

    init commit

---
+++ src/main/java/com/cragon/trainreboot/TrainReboot.java
@@ -0,0 +1,17 @@
+package com.cragon.trainreboot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TrainReboot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
